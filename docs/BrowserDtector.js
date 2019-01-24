d6e06df73e9f6c626676bfd00bd517ae2952ef36
sibiraj-s <dev.sibiraj@outlook.com>
2019-01-24 21:53:55 +0530

    docs: add support for unknown browsers

--- docs/BrowserDtector.js
+++ docs/BrowserDtector.js
@@ -6,15 +6,23 @@ class BrowserDtectorComponent extends React.PureComponent {
   render() {
     const browser = new BrowserDtector(window.navigator.userAgent);
     const userAgent = browser.parseUserAgent();
+    const isKnownBrowser = userAgent.name !== 'Unknown';
 
     return (
       <React.Fragment>
         <div className="content justify-center browser-dtector">
-          <p className="text">
-            You are viewing this page in -
-            <span className="browser-name">{'{'}{userAgent.name}{'}'}</span>
-          </p>
-          <p className="sub-text">{userAgent.platform}, v{userAgent.version}</p>
+          {isKnownBrowser && (
+            <React.Fragment>
+              <p className="text">
+                You are viewing this page in -
+                <span className="browser-name">{'{'}{userAgent.name}{'}'}</span>
+              </p>
+              <p className="sub-text">{userAgent.platform}, v{userAgent.version}</p>
+            </React.Fragment>
+          )}
+          {!isKnownBrowser && (
+            <p className="text"><span className="browser-name">{'{ Unknown Browser }'}</span></p>
+          )}
         </div>
         <footer className="footer">BrowserDtector: v{browser.__VERSION__}</footer>
       </React.Fragment>
