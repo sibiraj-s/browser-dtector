10897cb4ed3d2d4ddf5cba10bffff11e348e6988
sibiraj-s <dev.sibiraj@outlook.com>
2020-06-27 08:42:56 +0530

    ci: publish docs via workflow

--- scripts/publish-docs.js
+++ scripts/publish-docs.js
@@ -1,37 +1,41 @@
+const util = require('util');
+
 const inquirer = require('inquirer');
 const ghpages = require('gh-pages');
 const chalk = require('chalk');
-const argv = require('yargs-parser')(process.argv.slice(2));
 
-const packageJson = require('../package.json');
-
-if (!Object.keys(argv).some((method) => method === 'update' || method === 'release')) {
-  console.log(chalk.red('Error: Must Specify a relase method as arguments, either \'--update\' or \'--release\''));
-  process.exit();
-}
-
-const releaseMessage = `docs: ${packageJson.name} v${packageJson.version}`;
-const updateMessage = `docs: update ${new Date().toISOString()}`;
+const publishAsync = util.promisify(ghpages.publish);
 
 const ghPagesOptions = {
   branch: 'gh-pages',
-  message: argv.update ? updateMessage : releaseMessage,
+  message: `docs: update ${new Date().toISOString()}`,
 };
 
-inquirer.prompt([
+const questions = [
   {
     name: 'publishDocs',
     type: 'confirm',
     message: `Do You want to publish the docs to '${chalk.cyan(ghPagesOptions.branch)}' branch?`,
   },
-]).then((answers) => {
-  if (answers.publishDocs) {
-    ghpages.publish('dist/docs', ghPagesOptions, (err) => {
-      if (err) {
-        chalk.red('Unable to publish docs. Error: ', err);
+];
+
+async function publish() {
+  try {
+    if (!process.env.CI) {
+      const answers = await inquirer.prompt(questions);
+
+      if (!answers.publishDocs) {
         return;
       }
-      console.log(chalk.green(`\nDocumentation published successfully to ${chalk.cyan("'gh-pages'")} \n`));
-    });
+    } else {
+      console.log('CI detected. Skipping prompt.');
+    }
+
+    await publishAsync('dist/docs', ghPagesOptions);
+    console.log(chalk.green(`\nDocumentation published successfully to ${chalk.cyan('\'gh-pages\'')} \n`));
+  } catch (err) {
+    console.log(chalk.red('Unable to publish docs. Error:'), err);
   }
-});
+}
+
+publish();
