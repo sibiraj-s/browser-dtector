727f6056888e9f526b426d95538f69c3e59667e5
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2023-10-25 09:13:37 +0530

    test: fix skipped tests

--- specs/opera.spec.ts
+++ specs/opera.spec.ts
@@ -151,7 +151,7 @@ it('should detect `Opera` `v43` running in `Windows`', () => {
   assert(testData);
 });
 
-it.only('should detect `Opera Touch` `v4` running in `Ipad`', () => {
+it('should detect `Opera Touch` `v4` running in `Ipad`', () => {
   const testData = {
     userAgent: `Mozilla/5.0 (iPad; CPU OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)
                 Version/15.5 Mobile/15E148 Safari/604.1 OPT/4.0.1`,
@@ -170,7 +170,7 @@ it.only('should detect `Opera Touch` `v4` running in `Ipad`', () => {
   assert(testData);
 });
 
-it.only('should detect `Opera Touch` `v4` running in `IPhone`', () => {
+it('should detect `Opera Touch` `v4` running in `IPhone`', () => {
   const testData = {
     userAgent: `Mozilla/5.0 (iPhone; CPU iPhone OS 16_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)
                 Version/16.5 Mobile/15E148 Safari/604.1 OPT/4.0.1`,
@@ -189,7 +189,7 @@ it.only('should detect `Opera Touch` `v4` running in `IPhone`', () => {
   assert(testData);
 });
 
-it.only('should detect `Opera Touch` `v4` running in `Android`', () => {
+it('should detect `Opera Touch` `v4` running in `Android`', () => {
   const testData = {
     userAgent: 'Mozilla/5.0 (Linux; Android 11; SM-A115F Build/RP1A.200720.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.129 Mobile Safari/537.36 OPT/2.9',
     browserName: 'Opera',
